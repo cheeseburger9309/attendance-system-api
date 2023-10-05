333987b7fe91d331c7711fb3ad92fd9f2d6c405d
ganajayant <ganajayant28@gmail.com>
2023-10-05 16:54:24 +0530

    bug fixes and dockerized

--- server.py
+++ server.py
@@ -5,9 +5,11 @@ import threading
 
 import cv2
 import numpy as np
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/attendance', methods=['POST'])
@@ -18,7 +20,7 @@ def success():
             f.save('uploads/upload.jpg')
 
             recognizer = cv2.face.LBPHFaceRecognizer_create()
-            recognizer.read('recognizer/trainingData.yml')
+            recognizer.read('recognizer/model.yml')
             img = detect()
             if img is None:
                 return {
@@ -49,11 +51,8 @@ def success():
                     "message": "student attendance is marked"
                 }, 201
 
-            idresult = conn.execute(
-                'SELECT COUNT(*) FROM attendance').fetchone()
-            id = len(idresult) + 1
-            conn.execute('INSERT INTO attendance (id, student_id, date) VALUES (?, ?, ?)',
-                         (id, student_id, sqlite_date_format))
+            conn.execute('INSERT INTO attendance (student_id, date) VALUES (?, ?)',
+                         (student_id, sqlite_date_format))
             conn.commit()
             conn.close()
             return {
@@ -91,7 +90,13 @@ def upload():
                 return {
                     "message": "Invalid data"
                 }, 400
-
+            conn = get_db_connection()
+            result = conn.execute(
+                'SELECT * FROM students WHERE student_id = ?', (rollno,)).fetchone()
+            if result is not None:
+                return {
+                    "message": "User already exists"
+                }, 400
             for image, i in zip(images, range(10)):
                 image.save('uploads/upload'+str(i)+'.jpg')
                 face = detect('uploads/upload'+str(i)+'.jpg')
@@ -102,24 +107,13 @@ def upload():
                     cv2.imwrite(os.path.join(
                         user_directory, rollno + "_" + str(i) + '.jpg'), face)
 
-            conn = get_db_connection()
-            result = conn.execute(
-                'SELECT * FROM students WHERE student_id = ?', (rollno,)).fetchone()
-            if result is not None:
-                return {
-                    "message": "User already exists"
-                }, 400
-
-            result = conn.execute('SELECT COUNT(*) FROM students').fetchone()
-            id = len(result) + 1
-            conn.execute('INSERT INTO students (id, student_id, name, email) VALUES (?, ?, ?, ?)',
-                         (id, rollno, name, email))
+            conn.execute('INSERT INTO students (student_id, name, email) VALUES (?, ?, ?)',
+                         (rollno, name, email))
             conn.commit()
             conn.close()
 
             train_thread = threading.Thread(target=train_model)
             train_thread.start()
-
             return {
                 "message": "User added successfully & training started"
             }, 200
@@ -137,10 +131,16 @@ def detect(image='uploads/upload.jpg', cascade='cascades/haarcascade_frontalface
     faceCascade = cv2.CascadeClassifier(cascade)
     img = cv2.imread(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
     if faces is None or len(faces) == 0:
         return None
-    x, y, w, h = faces[0]
+    max_area = faces[0][2] * faces[0][3]
+    face = faces[0]
+    for f in faces:
+        if f[2] * f[3] > max_area:
+            max_area = f[2] * f[3]
+            face = f
+    x, y, w, h = face
     return img[y:y+h, x:x+w]
 
 
@@ -148,6 +148,8 @@ def train_model():
     recognizer = cv2.face.LBPHFaceRecognizer().create()
     conn = get_db_connection()
     result = conn.execute('SELECT * FROM students').fetchall()
+    conn.commit()
+    conn.close()
     if result is None or len(result) == 0:
         return
     faces = []
@@ -160,7 +162,6 @@ def train_model():
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
             faces.append(face)
             ids.append(row['id'])
-    conn.close()
     recognizer.train(faces, np.array(ids))
     recognizer.save('recognizer/model.yml')
 
@@ -169,3 +170,7 @@ def get_db_connection():
     conn = sqlite3.connect('db/database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
